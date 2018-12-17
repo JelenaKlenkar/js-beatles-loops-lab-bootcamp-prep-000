020a7fce5ad48be661be08a8694f7cf7dfdf745d
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-17 19:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var musicianString= [];
+  var musicianInstrument=[];
+  for(i=0; i<musian.lenght;i++){
+    musicianString= musicians[i] + " plays" + instruments[i];
+    musicianInstrument[i]=musicianString;
+  }
+  return musicianInstrument;
+}
+const facts=[
+'He was the last Beatle to learn to drive',
+'He was never a vegetarian',
+'He was a choir boy and boy scout',
+'He hated the sound of his own voice'
+];
