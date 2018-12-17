b1d941eb7bddc296c86166b709939279363bcccf
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-17 19:24:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function johnLennonFacts(factsArray){
 var i=0;
 var newArray=[];
   while(factsArray.length > i)
-  newArray.push(`${factsArray[i]}` + “!!!”);
+  newArray.push(`${factsArray[i]}` + "!!!");
 i++;
 }
