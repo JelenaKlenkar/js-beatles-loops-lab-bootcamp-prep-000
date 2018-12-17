7921949fa151f0c31f4af60d10add44698b95eda
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-17 19:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var musicianString= [];
   var musicianInstrument=[];
-  for(i=0; i<musician.lenght;i++){
+  for(i=0; i<musicians.lenght;i++){
     musicianString= musicians[i] + " plays" + instruments[i];
     musicianInstrument[i]=musicianString;
   }
